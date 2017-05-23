0028d40ab0a461f58c5bb7e087215378c2d6ed8a
Mike North <michael.l.north@gmail.com>
2017-05-23 15:00:10 +0200

    Linting errors

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -16,11 +16,8 @@ export default function(name, options = {}) {
     },
 
     afterEach() {
-      let afterEach =
-        options.afterEach && options.afterEach(...arguments);
-      return Promise.resolve(afterEach).then(() =>
-        destroyApp(this.application)
-      );
+      let afterEach = options.afterEach && options.afterEach(...arguments);
+      return Promise.resolve(afterEach).then(() => destroyApp(this.application));
     }
   });
 }
