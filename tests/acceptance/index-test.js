2df4870c79e730605458a063343451472773dd8f
Michael North <michael.l.north@gmail.com>
2018-01-02 23:27:31 -0800

    Ember 2.17

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -8,10 +8,20 @@ test('Installation verification', function(assert) {
   visit('/');
 
   andThen(function() {
-    assert.ok(find('#title').css('font-family').indexOf('Roboto') >= 0, 'Stylesheet is installed');
-    assert.equal(find('.material-icons').css('font-family'), '\'Material Icons\'', 'Icons are installed');
+    assert.ok(
+      find('#title')
+        .css('font-family')
+        .indexOf('Roboto') >= 0,
+      'Stylesheet is installed'
+    );
+    assert.equal(
+      find('.material-icons')
+        .css('font-family')
+        .replace(/['"]/g, ''),
+      'Material Icons',
+      'Icons are installed'
+    );
     assert.equal(typeof Materialize, 'object', 'JS is installed as an ES6 module');
     assert.equal(currentURL(), '/');
   });
 });
-
